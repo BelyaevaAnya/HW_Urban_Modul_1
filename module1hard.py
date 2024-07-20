5d7cb046387efb6b8695ef5f5d9841b688e76f05
Anya_Belyaeva <71212278+BelyaevaAnya@users.noreply.github.com>
2024-07-20 19:05:46 +0300

    Hw1

--- module1hard.py
+++ module1hard.py
@@ -1,15 +1,14 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
-
-
 def find_average_grade(lst):
-    return sum(lst) / len(lst)
+    return sum(lst)/len(lst)
 
+def get_dict_average_grade(students: set, grades: list)->dict:
+    avr_grade_dict = {}
+    students = sorted(students)
+    for index in range(len(students)):
+        avr_grade_dict[students[index]] = find_average_grade(grades[index])
+    return avr_grade_dict
 
-average_lst = []
-for grades_one_st in grades:
-    average_lst.append(find_average_grade(grades_one_st))
-
-avr_grade_dict = dict(zip(students, average_lst))
+avr_grade_dict = get_dict_average_grade(students, grades)
 print(avr_grade_dict, type(avr_grade_dict))
-# => {'Johnny': 4.0, 'Steve': 2.25, 'Khendrik': 4.0, 'Bilbo': 3.6666666666666665, 'Aaron': 4.8} <class 'dict'>
